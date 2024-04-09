857c1336ec0edd7cd19ca023f4901d714c0e1c40
Miguel Armenta <ma826r@att.com>
2024-04-09 15:12:21 -0500

    group/approle group component

--- samples/apps/autogen-studio-azure/frontend/src/components/header.tsx
+++ samples/apps/autogen-studio-azure/frontend/src/components/header.tsx
@@ -1,11 +1,13 @@
 import Icon from "./icons";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Listbox, Menu, Transition } from "@headlessui/react";
 import {
   XMarkIcon,
   Bars3Icon,
   BellIcon,
   MoonIcon,
   SunIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
 } from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 import { appContext } from "../hooks/provider";
@@ -58,6 +60,72 @@ const Header = ({ meta, link }: any) => {
       </appContext.Consumer>
     );
   };
+
+  const DropdownMenu = () => {
+    return (
+      <appContext.Consumer>
+        {(context: any) => {
+          // onst longestGroup = context.user?.groups?.reduce((a, b) => (a.length > b.length ? a : b), "");
+          return (
+            <div className="flex items-center group-dropdown" style={{width: `${100}`}}>
+              <span className="mr-2">Active Group:</span>
+              <div className="top-16 w-60" style={{width: `${175}px`}}>
+                <Listbox value={context.activeGroup} onChange={context.setActiveGroup}>
+                  <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                      <span className="block truncate">{context.activeGroup}</span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+                    <Transition
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                        {context.user?.groups?.map((group: string) => (
+                          <Listbox.Option
+                            key={group}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-green-100 text-green-900' : 'text-gray-900'
+                              }`
+                            }
+                            value={group}
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                    }`}
+                                >
+                                  {group}
+                                </span>
+                                {selected ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
+              </div>
+            </div>
+          );
+        }}
+      </appContext.Consumer>
+    )
+  };
+  
   return (
     <Disclosure
       as="nav"
@@ -128,6 +196,7 @@ const Header = ({ meta, link }: any) => {
               </div>
               {
                 <div className="hidden lg:ml-4 md:flex md:items-center">
+                  <DropdownMenu />
                   <DarkModeToggle />
 
                   {user && (
