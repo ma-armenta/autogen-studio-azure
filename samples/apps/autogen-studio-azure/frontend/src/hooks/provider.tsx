5ee44dc289b55ca9b5e771b28b597febab32e376
Miguel Armenta <ma826r@att.com>
2024-04-09 15:45:37 -0500

    Adding group and logout logic...

--- samples/apps/autogen-studio-azure/frontend/src/hooks/provider.tsx
+++ samples/apps/autogen-studio-azure/frontend/src/hooks/provider.tsx
@@ -5,9 +5,13 @@ import {
   setLocalStorage,
 } from "../components/utils";
 import { message } from "antd";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from '../authConfig';
 
 export interface IUser {
   name: string;
+  groups: string[];
   email?: string;
   username?: string;
   avatar_url?: string;
@@ -21,9 +25,12 @@ export interface AppContextType {
   cookie_name: string;
   darkMode: string;
   setDarkMode: any;
+  activeGroup: string | null;
+  setActiveGroup: any;
 }
 
 const cookie_name = "coral_app_cookie_";
+const msalInstance = new PublicClientApplication(msalConfig);
 
 export const appContext = React.createContext<AppContextType>(
   {} as AppContextType
@@ -34,39 +41,53 @@ const Provider = ({ children }: any) => {
     storedValue === null ? "light" : storedValue === "dark" ? "dark" : "light"
   );
 
-  const logout = () => {
-    // setUser(null);
-    // eraseCookie(cookie_name);
-    console.log("Please implement your own logout logic");
-    message.info("Please implement your own logout logic");
+  const storedValueActiveGroup = getLocalStorage("activeGroup", false);
+  const [activeGroup, setActiveGroup] = useState(
+    storedValueActiveGroup === null ? "Select group" : storedValueActiveGroup
+  );
+
+
+  const logout = async () => {
+    message.warning("Logging out... ");
+    await new Promise(f => setTimeout(f, 3000));
+    setUser(null);
+    msalInstance.logoutRedirect();
   };
+
+  const [user, setUser] = useState<IUser | null>(null);
 
   const updateDarkMode = (darkMode: string) => {
     setDarkMode(darkMode);
     setLocalStorage("darkmode", darkMode, false);
   };
 
-  // Modify logic here to add your own authentication
-  const initUser = {
-    name: "Guest User",
-    email: "guestuser@gmail.com",
-    username: "guestuser",
+  const updateUser = (user: IUser | null) => {
+    setUser(user);
+    // setLocalStorage("user", user, false);
   };
-  const [user, setUser] = useState<IUser | null>(initUser);
+
+  const updateActiveGruop = (newGroup: string) => {
+    setActiveGroup(newGroup);
+    setLocalStorage("activeGroup", newGroup, false);
+  };
 
   return (
-    <appContext.Provider
-      value={{
-        user,
-        setUser,
-        logout,
-        cookie_name,
-        darkMode,
-        setDarkMode: updateDarkMode,
-      }}
-    >
-      {children}
-    </appContext.Provider>
+    <MsalProvider instance={msalInstance}>
+      <appContext.Provider
+        value={{
+          user,
+          setUser: updateUser,
+          logout,
+          cookie_name,
+          darkMode,
+          setDarkMode: updateDarkMode,
+          activeGroup,
+          setActiveGroup: updateActiveGruop,
+        }}
+      >
+        {children}
+      </appContext.Provider>
+    </MsalProvider>
   );
 };
 
